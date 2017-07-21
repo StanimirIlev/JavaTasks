3a9be62f4338112f95f39cb0b3b1403ebf26ea82
Stanimir <stambeto2197@gmail.com>
2017-07-21 13:21:59 +0300

    Chapter: 2, Task: 5
    Made class Products for shopping
    Made sorted heterogeneous binary tree
    In class Test, made shopping list and then got element by its code
    Made classes Egg, Milk and Coffee which extends class Products

---
+++ src/com/stanimiriliev/chaptertwo/heterogeneoustree/products/Milk.java
@@ -0,0 +1,15 @@
+package com.stanimiriliev.chaptertwo.heterogeneoustree.products;
+
+/**
+ * @author Stanimir Iliev
+ * @since 21.07.2017
+ */
+public class Milk extends Products {
+
+    public final Packages pack;
+
+    public Milk(Packages pack, String make, Integer code){
+        super(make, code);
+        this.pack = pack;
+    }
+}
