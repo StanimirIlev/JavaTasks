a63bfbdbbb51fa06efe0d4b25eb2abbeac9fd97e
Stanimir <stambeto2197@gmail.com>
2017-07-22 22:31:02 +0300

    Chapter: Objects in Java(2), Task: Automobiles(2)
    Created abstract class Automobile with common properties for all cars
    Created classes VW, Mercedes and Opel which extends Automobiles
    Only class Opel has additional fields 'weight' and 'passengers'

---
+++ src/com/stanimiriliev/chaptertwo/auto/Test.java
@@ -0,0 +1,23 @@
+package com.stanimiriliev.chaptertwo.auto;
+
+import com.stanimiriliev.chaptertwo.auto.Automobile.Color;
+
+import java.util.Date;
+
+/**
+ * Test functionality of the classes VW, Mercedes and Opel
+ * @author Stanimir Iliev
+ * @since 22.07.2017
+ */
+public class Test {
+    public static void main(String[] args){
+
+        VW golf = new VW(180, Color.BLUE, new Date());
+        Mercedes eClass = new Mercedes(240, Color.BLACK, new Date());
+        
+        Opel astra = new Opel(190, Color.GREY, new Date(), 1100, 5);
+        Automobile vectra = new Opel(200, Color.BLACK, new Date(), 1240, 5);
+
+        System.out.println("VW Golf is color " + golf.color);
+    }
+}
